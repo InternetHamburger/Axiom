d229fe9eb2b65714dd076c3f7521bea01cb2ea20
Sindre Wolden <Sindre.Wolden@outlook.com>
2025-04-28 18:52:26 +0200

    Sprt: v2.0.5 vs 2.0.4 - More strict lmr
    Results of Axiom_new vs Axiom_old (10+0.1, NULL, 16MB, 8moves_v3.pgn):
    Elo: 31.00 +/- 15.44, nElo: 47.07 +/- 23.30
    LOS: 100.00 %, DrawRatio: 43.56 %, PairsRatio: 1.54
    Games: 854, Wins: 255, Losses: 179, Draws: 420, Points: 465.0 (54.45 %)
    Ptnml(0-2): [11, 84, 186, 110, 36], WL/DD Ratio: 0.65
    LLR: 2.89 (-2.25, 2.89) [0.00, 10.00]

--- Axiom/src/core/Search/Engine.cs
+++ Axiom/src/core/Search/Engine.cs
@@ -291,12 +291,20 @@ namespace Axiom.src.core.Search
                 }
                 else
                 {
-                    int reduction = (i >= 4 && depth >= 3 && !isCapture && !board.IsInCheck(board.WhiteToMove)) ? 1 : 0;
-                    score = -NegaMax(depth - 2 - reduction + extension, plyFromRoot + 1, -alpha - 1, -alpha);
-                    if (score > alpha)
+                    if (depth >= 2 && i >= 1)
                     {
-                        score = -NegaMax(depth - 1 - reduction + extension, plyFromRoot + 1, -beta, -alpha);
+                        int reduction = (i >= 4 && depth >= 3 && !isCapture && !board.IsInCheck(board.WhiteToMove)) ? 1 : 0;
+                        score = -NegaMax(depth - 2 - reduction + extension, plyFromRoot + 1, -alpha - 1, -alpha);
+                        if (score > alpha)
+                        {
+                            score = -NegaMax(depth - 1 - reduction + extension, plyFromRoot + 1, -beta, -alpha);
+                        }
                     }
+                    else
+                    {
+                        score = -NegaMax(depth - 1 + extension, plyFromRoot + 1, -beta, -alpha);
+                    }
+                    
                 }
 
                 board.UndoMove(move);
