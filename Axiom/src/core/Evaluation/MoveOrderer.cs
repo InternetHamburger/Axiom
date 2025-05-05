bbe16452d8707d0a5fb55ec80a18c8348230718d
wolden.sindre@gmail.com <wolden.sindre@gmail.com>
2025-05-05 11:51:36 +0200

    Nonreg: v2.0.9 vs 2.0.8 - Remove alpha hh updates + index hh with piece-to instead of capturedPiece-to
    Results of Axiom_new vs Axiom_old (10+0.1, NULL, 32MB, 8moves_v3.pgn):
    Elo: 15.27 +/- 13.45, nElo: 20.95 +/- 18.42
    LOS: 98.71 %, DrawRatio: 37.04 %, PairsRatio: 1.24
    Games: 1366, Wins: 431, Losses: 371, Draws: 564, Points: 713.0 (52.20 %)
    Ptnml(0-2): [43, 149, 253, 181, 57], WL/DD Ratio: 1.16
    LLR: 2.90 (-2.25, 2.89) [-10.00, 0.00]

--- Axiom/src/core/Evaluation/MoveOrderer.cs
+++ Axiom/src/core/Evaluation/MoveOrderer.cs
@@ -91,7 +91,7 @@ namespace Axiom.src.core.Evaluation
             else
             {
                 int index1 = board.WhiteToMove ? 0 : 1;
-                int index2 = board.Squares[move.TargetSquare];
+                int index2 = board.Squares[move.StartSquare];
                 int index3 = move.TargetSquare;
                 return HistoryTable[index1, index2, index3] - 10000;
             }
@@ -108,9 +108,9 @@ namespace Axiom.src.core.Evaluation
         public void UpdateHistoryTableBetaCutoff(Board.Board board, Move move, int depth)
         {
             int index1 = board.WhiteToMove ? 0 : 1;
-            int index2 = board.Squares[move.TargetSquare];
+            int index2 = board.Squares[move.StartSquare];
             int index3 = move.TargetSquare;
-            HistoryTable[index1, index2, index3] = Math.Clamp(HistoryTable[index1, index2, index3] + (3 * depth * depth), 0, 10000);
+            HistoryTable[index1, index2, index3] = Math.Clamp(HistoryTable[index1, index2, index3] + (depth * depth), 0, 10000);
         }
 
         public void UpdateHistoryTableAlphaRaise(Board.Board board, Move move, int depth)
