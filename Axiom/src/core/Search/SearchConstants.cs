424209e1a8c31edfa4df6d994f2426cb2421ce45
Sindre Wolden <Sindre.Wolden@outlook.com>
2025-05-18 20:32:21 +0200

    Use log formula in lmr

---
+++ Axiom/src/core/Search/SearchConstants.cs
@@ -0,0 +1,24 @@
+﻿using Axiom.src.core.Move_Generation;
+using Axiom.src.core.Search;
+
+namespace Axiom.src.core.Search
+{
+    static class SearchConstants
+    {
+        // Indexed by depth, i
+        public static int[,] LMR_TABLE;
+
+        static SearchConstants()
+        {
+            LMR_TABLE = new int[Engine.MaxPly + 1, MoveGenerator.MaxNumMoves + 1];
+            for (int i = 0; i < Engine.MaxPly + 1; i++)
+            {
+                for (int j = 0; j < MoveGenerator.MaxNumMoves + 1; j++)
+                {
+                    // Formula yoinked from heimdall
+                    LMR_TABLE[i, j] = (int)(0.8 + Math.Log(i) * Math.Log(j) * 0.4);
+                }
+            }
+        }
+    }
+}
